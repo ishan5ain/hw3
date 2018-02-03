9e0501848040b57faf28c39acebc7667adaeffd1
Ishan Sain <ishansain27@gmail.com>
2018-02-03 02:14:32 -0800

    added easing

--- lewitt-154.js
+++ lewitt-154.js
@@ -1,6 +1,10 @@
 //walldrawing-154
 //http://massmoca.org/event/walldrawing154/
 
+var rate = 0.1;
+var xe = 20;
+var xetarget = 70;
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -11,13 +15,22 @@ function draw() {
   stroke("#FA2F0A");
   noFill();
 
-  var x = map(mouseX, 0, width, width * 0.25, width * 0.75);
-  if (x <= width * 0.75) {
-    line(0, (height / 2) + 1, x, height / 2);
-    line(0, (height / 2) - 1, x, height / 2);
+  xe = xetarget * rate + xe * (1 - rate);
+	rect(20, 20, width-40, height-40);
+
+  if (xe <= width * 0.75) {
+    line(20, (height / 2) + 1, xe, height / 2);
+    line(20, (height / 2) - 1, xe, height / 2);
   } else {
-    line(0, (height / 2) + 1, width * 0.75, height / 2);
-    line(0, (height / 2) - 1, width * 0.75, height / 2);
+    line(20, (height / 2) + 1, width * 0.75, height / 2);
+    line(20, (height / 2) - 1, width * 0.75, height / 2);
   }
 
+	stroke(150);
+	rect(20, 20, width-40, height-40);
+}
+
+function mouseMoved() {
+  var x = map(mouseX, 0, width, width * 0.25, width * 0.75);
+  xetarget = x;
 }
