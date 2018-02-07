9b921e79038f0f5c501b49211de06f2f1e67e602
Ishan Sain <ishansain27@gmail.com>
2018-02-07 09:26:21 -0800

    optimized code

--- easing.js
+++ easing.js
@@ -6,8 +6,8 @@
 var rate = 0.1;
 var x = 0;
 var y = 0;
-var xtarget,
-  ytarget;
+// var xtarget,
+//   ytarget;
 
 function setup() {
   createCanvas(400, 400);
@@ -20,14 +20,9 @@ function draw() {
   noStroke();
   fill(180);
 
-  x = xtarget * rate + x * (1 - rate); // rate is 0.1, or 10% -- (1-rate) is 0.9, or 90%
-  y = ytarget * rate + y * (1 - rate);
+  x = mouseX * rate + x * (1 - rate); // rate is 0.1, or 10% -- (1-rate) is 0.9, or 90%
+  y = mouseY * rate + y * (1 - rate);
 
-  ellipse(x, y, 20, 20);
+  ellipse(x, y, 15, 15);
 
-}
-
-function mouseMoved() {
-  xtarget = mouseX;
-  ytarget = mouseY;
 }
